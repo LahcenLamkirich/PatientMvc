c1b887c1a21749a59a2e5bca06916086f79f42a7
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-04-02 14:02:13 +0000

    commit

--- src/main/java/com/lahcenmvc/patientmvc/Web/PatientController.java
+++ src/main/java/com/lahcenmvc/patientmvc/Web/PatientController.java
@@ -1,25 +1,18 @@
 package com.lahcenmvc.patientmvc.Web;
 
-import com.lahcenmvc.patientmvc.Entities.Patient;
 import com.lahcenmvc.patientmvc.Repositrories.PatientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/patients")
 public class PatientController {
-    @Autowired
-    PatientRepository patientRepository ;
 
-    @GetMapping()
-    public @ResponseBody
-    Iterable<Patient>
-    getAllPatients(){
-        return patientRepository.findAll();
+    private PatientRepository patientRepository ;
+
+    // l'injection de depandance avec le constructeur
+    public PatientController(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
     }
 
+    
 
 }
