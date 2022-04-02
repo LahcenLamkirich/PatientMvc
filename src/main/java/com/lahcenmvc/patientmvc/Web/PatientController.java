4fbf7e6aefc44034c369311a62d1ab8192af94ff
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-04-02 14:27:57 +0000

    commit

--- src/main/java/com/lahcenmvc/patientmvc/Web/PatientController.java
+++ src/main/java/com/lahcenmvc/patientmvc/Web/PatientController.java
@@ -1,9 +1,14 @@
 package com.lahcenmvc.patientmvc.Web;
 
+import com.lahcenmvc.patientmvc.Entities.Patient;
 import com.lahcenmvc.patientmvc.Repositrories.PatientRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -17,9 +22,15 @@ public class PatientController {
     }
 
     @GetMapping("")
-    public String getAllPatients(){
+    public String getAllPatients(Model model){
+        List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("patients", patients);
         return "patients";
     }
 
+    @GetMapping("/all")
+    public @ResponseBody String patient(){
+        return "Test";
+    }
 
 }
