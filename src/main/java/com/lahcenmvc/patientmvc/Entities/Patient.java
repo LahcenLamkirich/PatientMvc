869c1f032a0fa5e79484280713142ea7f644cb42
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-04-02 13:41:58 +0000

    commit:

--- src/main/java/com/lahcenmvc/patientmvc/Entities/Patient.java
+++ src/main/java/com/lahcenmvc/patientmvc/Entities/Patient.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Data
@@ -19,6 +17,8 @@ public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
     private String name ;
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance ;
     private boolean isMalade ;
     private int score ;
 
--- src/main/java/com/lahcenmvc/patientmvc/Web/PatientController.java
+++ src/main/java/com/lahcenmvc/patientmvc/Web/PatientController.java
@@ -21,5 +21,5 @@ public class PatientController {
         return patientRepository.findAll();
     }
 
-    
+
 }
