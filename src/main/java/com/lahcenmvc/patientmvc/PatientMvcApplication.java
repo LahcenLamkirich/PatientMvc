029d6e039cdeabc30a7d05401c1544a14cbcb753
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-04-04 10:10:30 +0000

    commit

--- src/main/java/com/lahcenmvc/patientmvc/PatientMvcApplication.java
+++ src/main/java/com/lahcenmvc/patientmvc/PatientMvcApplication.java
@@ -25,11 +25,11 @@ public class PatientMvcApplication {
     @Bean
     CommandLineRunner start() {
         return args -> {
-            patientRepository.save(new Patient(null,"Lahcen", new Date(), false, 255));
-            patientRepository.save(new Patient(null,"Hassan", new Date(), false, 120));
-            patientRepository.save(new Patient(null,"Yassine", new Date(), true, 562));
-            patientRepository.save(new Patient(null,"Khaled", new Date(), false, 780));
-            patientRepository.save(new Patient(null,"Rabii", new Date(), true, 128));
+//            patientRepository.save(new Patient(null,"Lahcen", new Date(), false, 255));
+//            patientRepository.save(new Patient(null,"Hassan", new Date(), false, 120));
+//            patientRepository.save(new Patient(null,"Yassine", new Date(), true, 562));
+//            patientRepository.save(new Patient(null,"Khaled", new Date(), false, 780));
+//            patientRepository.save(new Patient(null,"Rabii", new Date(), true, 128));
         };
     }
 
